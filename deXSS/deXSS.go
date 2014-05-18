27b5a740b020aea8dc8aacbb3c0410958b877f40
Mikko Lehtisalo <mikkolehtisalo@gmail.com>
2014-05-18 20:36:53 +0300

    Implementation of HTML stripping

--- deXSS/deXSS.go
+++ deXSS/deXSS.go
@@ -4,18 +4,108 @@ import (
     "code.google.com/p/go.net/html"
     "github.com/revel/revel"
     "bytes"
+    "regexp"
+    "strings"
 )
 
-// Key of allowed is tag, its value is comma separated list of allowed attributes for that tag
-func FilterHTML(h string, allowed map[string]string) {
+var (
+    stripped_content *regexp.Regexp = regexp.MustCompile("<html><head></head><body>(.*)</body></html>")
+)
+
+// Strips the html/head/body tags
+func strip_html(in string) string {
+    out := in
+    if stripped_content.MatchString(in) {  
+        out = stripped_content.FindStringSubmatch(in)[1]
+
+    }
+    return out
+}
+
+// Render the document or node to string
+func get_html(n *html.Node) string {
+    tmp := bytes.Buffer{}
+    html.Render(&tmp, n)
+    return tmp.String()
+}
+
+// Check whether node can be found from the allowed map
+func is_legal_node(n *html.Node, allowed map[string]string) bool {
+    legal := false
+    _, present := allowed[n.Data]
+    if present {
+        legal = true
+    }
+    return legal
+}
+
+// Remove node from document
+func remove_node(n *html.Node) {
+    revel.TRACE.Printf("remove_node() %+v", n)
+    var parent *html.Node
+    // Nil if root node, but probably don't need to handle that
+    parent = n.Parent
+    parent.RemoveChild(n)
+}
+
+func filter_attributes(n *html.Node, allowed map[string]string) {
+    revel.TRACE.Printf("filter_attributes() %+v, %+v", n, allowed)
+    result := []html.Attribute{}
+    // Loop all the node's attributes against the allowed list for this node
+    for _, att := range n.Attr {
+        legal := false
+        for _, okay := range strings.Split(allowed[n.Data],",") {
+            if att.Key == okay {
+                legal = true
+            }
+        }
+        // The attribute seems ok, add it to results
+        if legal {
+            result = append(result, att)
+        }
+    }
+    revel.TRACE.Printf("filter_attributes() result %+v", result)
+    n.Attr = result
+
+}
+
+// Filters HTML, returns filtered version. Please note that go.net/htmls parsing might change many minor things in document.
+// Key of allowed is tag, its value is comma separated list of allowed attributes for that tag.
+// If stip is set, removes the head/body/html tags that html.Parse always ensures in results.
+func FilterHTML(h string, allowed map[string]string, strip bool) string {
+    revel.TRACE.Printf("FilterHTML() %+v, %+v", h, allowed)
     meh := bytes.NewBufferString(h)
     doc, err := html.Parse(meh)
-
     if err != nil {
         revel.ERROR.Printf("Unable to parse HTML: %+v", err)
     }
 
-    revel.ERROR.Printf("doc: %+v\n", doc)
+    var f func(*html.Node)
+    f = func(n *html.Node) {
+        if n.Type == html.ElementNode {
+            // Filter only ElementNodes
+            if !is_legal_node(n, allowed) {
+                remove_node(n)
+            } else {
+                // Still have to filter Attributes
+                filter_attributes(n, allowed)
+            }
+        }
+        for c := n.FirstChild; c != nil; c = c.NextSibling {
+            f(c)
+        }
+    }
 
+    f(doc)
 
+    // Strip html/head/body?
+    var result string
+    if strip {
+        result = strip_html(get_html(doc))
+    } else {
+        result = get_html(doc)
+    }
+    revel.TRACE.Printf("FilterHTML() returning %+v", result)
+
+    return result
 }
--- deXSS/deXSS_test.go
+++ deXSS/deXSS_test.go
@@ -7,7 +7,7 @@ var (
 )
 
 func init() {
-    allowed := make(map[string]string)
+    allowed = make(map[string]string)
     allowed["p"] = "class,id"
     allowed["div"] = "class,id"
     allowed["h1"] = "class,id"
@@ -18,46 +18,82 @@ func init() {
     allowed["a"] = "class,id,href,rel"
     allowed["img"] = "class,id,src,alt,hspace,vspace,width,height"
     allowed["span"] = "class,id,style"
+    allowed["html"] = ""
+    allowed["head"] = ""
+    allowed["body"] = ""
 }
 
-func TestEmpty(t *testing.T) {
-    out := FilterHTML("", allowed)
+
+// strip_html
+
+func TestStripEmpty(t *testing.T) {
+    out := strip_html("")
     if out != "" {
-        t.Error("Expected empty string, got ", out)
+        t.Error("Expected empty string, got", out)
+    }
+}
+
+func TestStripBasic(t *testing.T) {
+    out := strip_html("<html><head></head><body></body></html>")
+    if out != "" {
+        t.Error("Expected empty string, got", out)
+    }
+}
+
+func TestStripBasicContent(t *testing.T) {
+    out := strip_html("<html><head></head><body>Pills!</body></html>")
+    if out != "Pills!" {
+        t.Error("Expected Pills!, got", out)
+    }
+}
+
+// FilterHTML
+
+func TestEmpty(t *testing.T) {
+    out := FilterHTML("", allowed, true)
+    if out != "" {
+        t.Error("Expected empty string, got", out)
+    }
+}
+
+func TestEmptyNoStrip(t *testing.T) {
+    out := FilterHTML("", allowed, false)
+    if out != "<html><head></head><body></body></html>" {
+        t.Error("Expected empty string, got", out)
     }
 }
 
 func TestText(t *testing.T) {
-    out := FilterHTML("The grass is always greener on the other side of the force", allowed)
+    out := FilterHTML("The grass is always greener on the other side of the force", allowed, true)
     if out != "The grass is always greener on the other side of the force" {
-        t.Error("Expected sample string, got ", out)
+        t.Error("Expected sample string, got", out)
     }
 }
 
 func TestLegalTags(t *testing.T) {
-    out := FilterHTML("<p>Hello <a href=\"/snake\">badger</a>!</p><p>Got it?</p>", allowed)
+    out := FilterHTML("<p>Hello <a href=\"/snake\">badger</a>!</p><p>Got it?</p>", allowed, true)
     if out != "<p>Hello <a href=\"/snake\">badger</a>!</p><p>Got it?</p>" {
-        t.Error("Expected back the same string, got ", out)
+        t.Error("Expected back the same string, got", out)
     }
 }
 
 func TestIllegalTag(t *testing.T) {
-    out := FilterHTML("And how <script>alert(\"Surprise!\");</script> it works!", allowed)
-    if out != "And how it works!" {
-        t.Error("Expected tag cleanly gone, got ", out)
+    out := FilterHTML("And how <script>alert(\"Surprise!\");</script> it works!", allowed, true)
+    if out != "And how  it works!" {
+        t.Error("Expected tag cleanly gone, got", out)
     }
 }
 
 func TestIllegalAttribute(t *testing.T) {
-    out := FilterHTML("<p>Hello <a mushroom=\"big\" href=\"/snake\">badger</a>!</p><p>Got it?</p>", allowed)
+    out := FilterHTML("<p>Hello <a mushroom=\"big\" href=\"/snake\">badger</a>!</p><p>Got it?</p>", allowed, true)
     if out != "<p>Hello <a href=\"/snake\">badger</a>!</p><p>Got it?</p>" {
-        t.Error("Expected back the string without mushrooms, got ", out)
+        t.Error("Expected back the string without mushrooms, got", out)
     }
 }
 
 func TestIllegalNestedTag(t *testing.T) {
-    out := FilterHTML("<p>And how <script>alert(\"Surprise!\");</script> it works!</p>", allowed)
-        if out != "<p>And how it works!</p>" {
-            t.Error("Expected tag cleanly gone, got ", out)
-        }
+    out := FilterHTML("<p>And how <script>alert(\"Surprise!\");</script> it works!</p>", allowed, true)
+    if out != "<p>And how  it works!</p>" {
+        t.Error("Expected tag cleanly gone, got", out)
     }
+}
